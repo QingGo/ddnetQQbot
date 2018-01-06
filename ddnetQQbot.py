964719cf9496336390f9c438aa2c69ca0dafe511
QingGo <zyqingjohn@qq.com>
2018-01-06 16:38:08 +0800

    把聊天功能限制在闲聊群

--- ddnetQQbot.py
+++ ddnetQQbot.py
@@ -8,10 +8,12 @@ import json
 bot.Login(['-q', '2143738142'])
 
 #这里改为你的群名
-g = bot.List('group', 'TeeWorlds中国社区')[0]
+mainGroup = bot.List('group', 'TeeWorlds中国社区')[0] #2960233702
+chatGroup = bot.List('group', 'TeeWorlds闲聊群')[0] #1516349281
+isChatGroup = False
 
 #for test
-#g = bot.List('group')[1]
+#mainGroup = bot.List('group')[1]
 
 replyFile = "autoReply.txt"
 replyDict = {}
@@ -30,22 +32,31 @@ requestJson = {"key": "692b5c941e7a43e2be89b1047b605049","info": "", "userid":""
 
 while True:
     time.sleep(2)
-    fromType, fromNumber, groupNumber, content = bot.poll()
-    print (fromType, fromNumber, groupNumber, content)
+    fromType, groupNumber, fromNumber, content = bot.poll()
+    print (fromType, groupNumber, fromNumber, content)
     keywordInContent = False
+    if groupNumber == "2960233702":
+        sendtoGroup = mainGroup
+        isChatGroup = False
+    elif groupNumber == "1516349281":
+        sendtoGroup = mainGroup
+        isChatGroup = True
     #这里改为你的ID
     if "@brainfullyTEE" in content:
         print ("@我的消息")
         if "?" in content or "？" in content:
             for keyword in replyDict:
                 if keyword.lower() in content.lower():
-                    bot.SendTo(g, replyDict[keyword])
+                    bot.SendTo(sendtoGroup, replyDict[keyword])
                     keywordInContent = True
             if not keywordInContent:
-                bot.SendTo(g, "不好意思，你所说的关键词尚未收录。快去https://github.com/QingGo/ddnetQQbot 贡献词库吧。如果要进行普通对话请不要带问号。")
+                bot.SendTo(sendtoGroup, "不好意思，你所说的关键词尚未收录。快去https://github.com/QingGo/ddnetQQbot 贡献词库吧。如果要进行普通对话请不要带问号。")
         else:
-            requestJson["info"] = content.replace("@brainfullyTEE ","")
-            requestJson["userid"] = groupNumber
-            respone = requests.post(chatAPI, requestJson)
-            responeContent = json.loads(respone.text)
-            bot.SendTo(g, responeContent["text"]+responeContent.get("url", ""))
+            if isChatGroup:
+                requestJson["info"] = content.replace("@brainfullyTEE ","")
+                requestJson["userid"] = groupNumber
+                respone = requests.post(chatAPI, requestJson)
+                responeContent = json.loads(respone.text)
+                bot.SendTo(sendtoGroup, responeContent["text"]+responeContent.get("url", ""))
+            else:
+                bot.SendTo(sendtoGroup, "询问关键词的话请加上问号")
